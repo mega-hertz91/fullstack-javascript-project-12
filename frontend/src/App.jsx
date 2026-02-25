707b65742d6944bce4e15b5d97866f28be29ce3f
Игорь Бабушкин <ibabushkin@elma-bpm.com>
2026-02-25 12:08:28 +0400

    add 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainPage, LoginPage } from './components/pages';
+import { MainPage, LoginPage, NotFoundPage } from './components/pages';
+
+const routes = [
+  {
+    path: '',
+    index: true,
+    element: <MainPage />
+  },
+  {
+    path: 'login',
+    element: <LoginPage />
+  },
+  
+  // 404 page should be the last route
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
+]
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<MainPage />} />
-        <Route path="login" element={<LoginPage />} />
+        {routes.map((route, index) => <Route key={index} {...route} />)}
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/pages/index.js
@@ -0,0 +1,3 @@
+export { default as LoginPage } from './login.page.jsx';
+export { default as MainPage } from './main.page.jsx';
+export { default as NotFoundPage } from './not-found.page.jsx';
