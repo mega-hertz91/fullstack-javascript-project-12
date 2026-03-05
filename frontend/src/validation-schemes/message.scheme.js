56e338db7869ee53eba55d58d7b3cb163b3569b1
ibabushkin <ibabushkin@elma-bpm.com>
2026-03-05 23:59:45 +0400

    add validate message

--- frontend/src/validation-schemes/message.scheme.js
+++ frontend/src/validation-schemes/message.scheme.js
@@ -3,7 +3,8 @@ import i18n from "@/i18n";
 
 export default Yup.object().shape({
   body: Yup.string()
-    .min(1, i18n.t("fields.min"))
-    .max(1000, i18n.t("fields.max"))
-    .required(i18n.t("fields.requered")),
+    .min(1, i18n.t("fields.range", { min: 1, max: 1000 }))
+    .max(1000, i18n.t("fields.range", { min: 1, max: 1000 }))
+    .required(i18n.t("fields.requered"))
+    .trim(i18n.t("fields.requered")),
 });
