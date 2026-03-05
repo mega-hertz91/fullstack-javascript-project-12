9659634e015664fddc16f1e1bdf827876b57ca01
Игорь Бабушкин <ibabushkin@elma-bpm.com>
2026-03-05 10:31:13 +0400

    delete login scheme

--- frontend/src/validation-schemes/index.js
+++ frontend/src/validation-schemes/index.js
@@ -1,4 +1,3 @@
-export { default as loginScheme } from "./login.scheme";
 export { default as signupScheme } from "./signup.scheme";
 export { default as messageScheme } from "./message.scheme";
 export { default as channelScheme } from "./channel.scheme";
