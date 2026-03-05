1dcd985c40c8b33e6603d21d109dde523724f7d3
ibabushkin <ibabushkin@elma-bpm.com>
2026-03-05 20:27:05 +0400

    fix/labe; form create channel

---
+++ frontend/src/utils/profanity.util.js
@@ -0,0 +1,30 @@
+import leoProfanity from "leo-profanity";
+
+let isProfanityInitialized = false;
+
+export const initProfanity = (languages = ["ru", "en"]) => {
+  if (isProfanityInitialized) {
+    return leoProfanity;
+  }
+
+  if (!Array.isArray(languages) || languages.length === 0) {
+    leoProfanity.loadDictionary();
+    isProfanityInitialized = true;
+
+    return leoProfanity;
+  }
+
+  const [baseLanguage, ...additionalLanguages] = languages;
+
+  leoProfanity.loadDictionary(baseLanguage);
+
+  additionalLanguages.forEach((language) => {
+    leoProfanity.add(leoProfanity.getDictionary(language));
+  });
+
+  isProfanityInitialized = true;
+
+  return leoProfanity;
+};
+
+export { leoProfanity };
