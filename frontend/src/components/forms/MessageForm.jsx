0f39d85c3aad8373c8938fcb5c0c41897cf2af77
ibabushkin <ibabushkin@elma-bpm.com>
2026-03-06 11:58:24 +0400

    fix last

--- frontend/src/components/forms/MessageForm.jsx
+++ frontend/src/components/forms/MessageForm.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useRef } from 'react'
-import { useFormik } from 'formik'
-import { messageScheme } from '@/validation-schemes/'
-import { useTranslation } from 'react-i18next'
-import { initProfanity, leoProfanity } from '@/utils/profanity.util'
+import { useEffect, useRef } from "react";
+import { useFormik } from "formik";
+import { messageScheme } from "@/validation-schemes/";
+import { useTranslation } from "react-i18next";
+import { initProfanity, leoProfanity } from "@/utils/profanity.util";
 
-import { Form, Button, Spinner, CloseButton, Fade } from 'react-bootstrap'
+import { Form, Button, Spinner, CloseButton, Fade } from "react-bootstrap";
 
-initProfanity(['ru', 'en'])
+initProfanity(["ru", "en"]);
 
 const MessageForm = ({ onSubmit, initialValues }) => {
-  const { t } = useTranslation()
-  const inputRef = useRef(null)
+  const { t } = useTranslation();
+  const inputRef = useRef(null);
   /**
    * Formik form for handling message input and submission. On submit, it sends the message to the server and refetches the messages to update the chat list.
    */
@@ -18,26 +18,26 @@ const MessageForm = ({ onSubmit, initialValues }) => {
     initialValues,
     validationSchema: messageScheme,
     onSubmit: async (values, { resetForm }) => {
-      const isNotCorrect = leoProfanity.check(values.body)
+      const isNotCorrect = leoProfanity.check(values.body);
 
       const data = {
         ...values,
         body: !isNotCorrect ? values.body : leoProfanity.clean(values.body),
-      }
+      };
 
-      await onSubmit(data)
-      resetForm()
+      await onSubmit(data);
+      resetForm();
     },
-  })
+  });
 
-  const onKeyEvent = e => {
-    const { key } = e
+  const onKeyEvent = (e) => {
+    const { key } = e;
 
-    if (key === 'Enter' && messageForm.values.body) {
-      e.preventDefault()
-      messageForm.handleSubmit()
+    if (key === "Enter" && messageForm.values.body) {
+      e.preventDefault();
+      messageForm.handleSubmit();
     }
-  }
+  };
 
   /**
    * Reaction to changes in initialValues props. When they change, it updates the form values accordingly. This is useful for editing messages, where the form needs to be populated with the existing message data when the user clicks "edit".
@@ -51,13 +51,13 @@ const MessageForm = ({ onSubmit, initialValues }) => {
         <Form.Group className="w-100">
           <Form.Control
             ref={inputRef}
-            placeholder={t('chatList.typeYouMessage')}
+            placeholder={t("chatList.typeYouMessage")}
             name="body"
             value={messageForm.values.body}
             onChange={messageForm.handleChange}
             isInvalid={!!messageForm.errors.body && messageForm.touched.body}
             onKeyDown={onKeyEvent}
-            aria-label={t('chatList.newMessage')}
+            aria-label={t("chatList.newMessage")}
           />
           <Form.Control.Feedback
             type="invalid"
@@ -66,7 +66,7 @@ const MessageForm = ({ onSubmit, initialValues }) => {
             {messageForm.errors.body}
           </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group>  
+        <Form.Group>
           <Button
             variant="primary"
             type="submit"
@@ -80,12 +80,12 @@ const MessageForm = ({ onSubmit, initialValues }) => {
                 className="me-2 mt-1 bg-transparent"
               />
             )}
-            <span>{'>>'}</span>
+            <span>{">>"}</span>
           </Button>
         </Form.Group>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default MessageForm
+export default MessageForm;
